f6eb44ca7d77ac4fecf30028f8dacf252768a811
chykong <chykong@163.com>
2017-05-18 13:42:41 +0800

    Spring+redis实现session共享

--- chapter3_3_redis_spring_session/src/main/java/com/critc/controller/IndexController.java
+++ chapter3_3_redis_spring_session/src/main/java/com/critc/controller/IndexController.java
@@ -1,7 +1,6 @@
 package com.critc.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,20 +11,35 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping("/")
 public class IndexController {
-
+    /**
+     * 模拟进度登录界面
+     *
+     * @param request
+     * @return
+     */
     @RequestMapping(value = "login")
-    public String login(HttpServletRequest request, String username) {
+    public String login(HttpServletRequest request) {
         return "login";
     }
 
+    /**
+     * 模拟登录
+     * @param request
+     * @return
+     */
     @RequestMapping(value = "checkLogin")
-    public String checkLogin(HttpServletRequest request, String username) {
+    public String checkLogin(HttpServletRequest request) {
         request.getSession().setAttribute("user", "admin");
         return "redirect:/index.htm";
     }
 
+    /**
+     * 模拟进入首页
+     * @param request
+     * @return
+     */
     @RequestMapping(value = "index")
-    public String index(HttpServletRequest request, Model model) {
+    public String index(HttpServletRequest request) {
         System.out.println(request.getSession().getAttribute("user"));
         return "index";
     }
